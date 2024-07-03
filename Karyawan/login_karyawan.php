682e51b8000b610b01601a4102e93d61be16e39a
kaka <22523129@students.uii.ac.id>
2024-07-03 16:05:47 +0700

    login karyawan

--- Karyawan/login_karyawan.php
+++ Karyawan/login_karyawan.php
@@ -18,14 +18,17 @@
         </div>
         <div class="login">
             <form action="login.php" method="post">
-                <label for="username">Username:</label><br>
-                <input type="text" id="username" name="username" required><br>
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" required><br><br>
                 
-                <label for="password">Password:</label><br>
+                <label for="password">Password:</label>
                 <input type="password" id="password" name="password" required><br><br>
+               
+                <input  type="submit" value="Login">
+               
                 
-                <input type="submit" value="Login">
             </form>
+            
         </div>
 </body>
 </html>
